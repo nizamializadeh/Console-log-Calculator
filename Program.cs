b7a89870ca8e1e64e9fcb7dcb65fce554a5ec051
Nizami <nizamializade0@gmail.com>
2017-08-11 19:55:19 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("                         **** CALCULATOR **** \n\nBir Emeliyat Secin \n\n1.Toplama\n2.Cixma \n3.Vurma \n4.Bolme\n");
+            var a = Console.ReadLine();
+            Console.WriteLine("1-ci Reqemi yazin");
+            var b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("2-ci Reqemi yazin");
+            var c = Convert.ToInt32(Console.ReadLine());
+            if (a == "Toplama")
+            {
+                Console.WriteLine(b+c);
+            }
+            if (a == "Cixma")
+            {
+                Console.WriteLine(b - c);
+            }
+            if (a == "Vurma")
+            {
+                Console.WriteLine(b * c);
+            }
+            if (a == "Bolme")
+            {
+                Console.WriteLine(b / c);
+            }
+            else
+            {
+                Console.WriteLine("Xahis Edirik Duzgun Reqem Yazin");
+            }
+        }
+    }
+}

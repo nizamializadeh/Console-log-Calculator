78a8f5bb6ff04c4b9bca585a787cfd16c0329664
Nizami <nizamializade0@gmail.com>
2017-08-16 00:24:57 +0400

    Add files via upload

---
+++ ConsoleApp16/ConsoleApp16/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] taxi = { 30,120,100,130, 20 };
+            int[] musderi = new int[5];
+            for (int i = 0; i <5; i++)
+            {
+                Console.WriteLine("cordinatlarinizi girin:");
+                musderi[i] = Convert.ToInt16(Console.ReadLine());
+                int enkicikm;
+                enkicikm = Math.Abs(taxi[0] - musderi[i]);
+                    for (int j = 1; j < 5; j++)
+                    {
+                    if (enkicikm>= Math.Abs(taxi[j] - musderi[i]))
+                    {
+                        enkicikm = Math.Abs(taxi[j] - musderi[i]);
+                    }
+                    else if (j>3)
+                    {
+                        Console.WriteLine(taxi[j]);
+                    }
+                   
+                
+                }
+                Console.WriteLine(enkicikm+ musderi[i]);
+               
+
+            }
+        }
+    }
+}
---
+++ ConsoleApp17/ConsoleApp17/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Calculator cem = new Calculator();
+                Calculator cix = new Calculator();
+                Calculator vur = new Calculator();
+                Calculator bol = new Calculator();
+                Console.WriteLine("Emeliyat Secin: ");
+                var operation = Console.ReadLine();
+                if (operation == "+")
+                {
+                    Console.Write("1 ci Eded  ");
+                    var z = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("2 ci Eded  ");
+                    var e = Convert.ToInt32(Console.ReadLine());
+                    var ce = cem.Toplama(z, e);
+                    Console.WriteLine("cavab: " + ce);
+                }
+                else if (operation == "-")
+                {
+                    Console.Write("1 ci Eded  ");
+                    var z = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("2 ci Eded  ");
+                    var e = Convert.ToInt32(Console.ReadLine());
+                    var ce = cix.Cixma(z, e);
+                    Console.WriteLine("cavab: " + ce);
+                }
+                else if (operation == "*")
+                {
+                    Console.Write("1 ci Eded  ");
+                    var z = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("2 ci Eded  ");
+                    var e = Convert.ToInt32(Console.ReadLine());
+                    var ce = vur.Vurma(z, e);
+                    Console.WriteLine("cavab: " + ce);
+                }
+                else if (operation == "/")
+                {
+                    Console.Write("1 ci Eded  ");
+                    var z = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("2 ci Eded  ");
+                    var e = Convert.ToInt32(Console.ReadLine());
+                    var ce = bol.Bolme(z, e);
+                    Console.WriteLine("cavab: " + ce);
+                }
+                else
+                {
+                    Console.WriteLine("duzgun emeliyat secin +,-,*,/");
+                }
+
+            }
+            catch (Exception )
+            {
+                Console.WriteLine("xahis edirik yeniden cehd edin :)");
+                
+            }
+           
+
+
+        }
+        class Calculator
+        {
+            public int Toplama(int a, int b)
+            {
+                return a + b;
+            }
+            public int Cixma(int a, int b)
+            {
+                return a - b;
+            }
+            public int Vurma(int a, int b)
+            {
+                return a * b;
+            }
+            public int Bolme(int a, int b)
+            {
+                return a / b;
+            }
+        }
+    }
+    }
+

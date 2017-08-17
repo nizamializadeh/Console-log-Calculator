0fe7d1de25278a5be106342a5d3132f435ca66f0
Nizami <nizamializade0@gmail.com>
2017-08-17 22:25:32 +0400

    Add files via upload

---
+++ ConsoleApp21/ConsoleApp21/Masin.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp21
+{
+    class Masin
+    {
+        public string Model;
+        public string Reng;
+        public string Motor;
+        public string Ili;
+        public int Suret;
+
+        public int  Suretlendir(int srtartr)
+        {
+            Suret += srtartr;
+            //Console.WriteLine("Sizin Suretiiniz " + Suret);
+            return Suret;
+        }
+        public int  Yavaslad(int srtrazalt)
+        {
+            Suret -= srtrazalt;
+            //Console.WriteLine("Yavaslayirsiniz Sizin Suretiniz" + Suret);
+            return Suret;
+        }
+        public string Suretcontrol()
+        {
+            string msj = " ";
+            if (Suret>60)
+            {
+                msj = "suret coxdur diqetli olun ";
+            }
+            else
+            {
+                msj = "normal suret";
+            }
+            return msj;
+        }
+    }
+}
---
+++ ConsoleApp21/ConsoleApp21/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Masin car = new Masin();
+            int masinsureti = 0;
+            masinsureti = car.Suretlendir(101);
+            car.Suretlendir(20);
+            car.Suretlendir(23);
+            car.Suretlendir(4);
+            car.Suretlendir(12);
+            car.Suretlendir(5);
+            masinsureti = car.Yavaslad(50);
+            car.Model = "Mercedes";
+            car.Ili = "23";
+            car.Motor = "6.5";      
+            car.Reng = "White";
+           
+            Console.WriteLine("Masnin ozelikleri \nmodeli {0},Ili {1}, Motoru {2}, rengi {3} ",car.Model,car.Ili,car.Motor,car.Reng);
+            Console.WriteLine(" suret " + masinsureti);
+            Console.WriteLine(car.Suretcontrol());
+        }
+    }
+}
